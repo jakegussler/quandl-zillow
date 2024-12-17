dc6b8c7c6806cf00095cbeeef9ee93825c5bb54b
Jake Gussler <jakegussler@gmail.com>
2024-12-16 22:35:12 -0500

    add env variables

--- scripts/setup_database.sh
+++ scripts/setup_database.sh
@@ -1,12 +1,30 @@
 #!/bin/bash
 
-#Database variables
-DB_NAME="zillow_analytics"
-DB_USER="zillow_user"
-DB_PASSWORD="zillow_password"
+#Get environment variables from .env file
+ENV_FILE="${1:-../.env}"
+
+if [ -f "$ENV_FILE" ]; then
+    source "$ENV_FILE"
+else
+    echo ".env file not found in $ENV_FILE, exiting"
+    return 1
+fi
+
+#Set environment variables
+DB_NAME=${POSTGRES_DATABASE}
+DB_USER=${POSTGRES_USER}
+DB_PASSWORD=${POSTGRES_PASSWORD}
+POSTGRES_SUPERUSER_PASSWORD=${POSTGRES_SUPERUSER_PASSWORD}
+
+export PGPASSWORD="$POSTGRES_SUPERUSER_PASSWORD"
+
+echo "$DB_NAME"
+echo "$DB_USER"
+echo "$DB_PASSWORD"
+echo "$POSTGRES_SUPERUSER_PASSWORD"
 
 #Connect to postgres as super user and run commands
-psql -U postgres <<EOF
+psql -U postgres -h localhost -w <<EOF
 
 --Drop database if exists
 DROP DATABASE IF EXISTS $DB_NAME;
@@ -24,4 +42,5 @@ CREATE USER $DB_USER WITH PASSWORD '$DB_PASSWORD';
 GRANT ALL PRIVILEGES ON DATABASE $DB_NAME TO $DB_USER;
 EOF
 
+unset PGPASSWORD
 echo "database and user setup complete"
