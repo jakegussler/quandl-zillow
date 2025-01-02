f3b2fe799ff33bbaffd289feecea6e4359fe2a17
Jake Gussler <jakegussler@gmail.com>
2025-01-01 23:48:58 -0500

    update imports

--- src/utils/db_utils.py
+++ src/utils/db_utils.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
-from config import DB_CONFIG
+from utils.config import DB_CONFIG
 
 def get_engine():
     connection_string = f'{DB_CONFIG["type"]}://{DB_CONFIG["user"]}:{DB_CONFIG["password"]}@{DB_CONFIG["host"]}:{DB_CONFIG["port"]}/{DB_CONFIG["database"]}'
